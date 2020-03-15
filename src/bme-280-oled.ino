c29d3fede1ff6a6699d6bc244c25a3d55934a18a
Conrad Eggan <conrade@redcatmfg.com>
2020-03-15 17:55:03 -0500

    dewpoint as function

--- src/bme-280-oled.ino
+++ src/bme-280-oled.ino
@@ -25,7 +25,7 @@ Adafruit_SSD1306 display(-1);
  int led1 = D7;
 
 // Display update interval in seconds
-const int updatePeriod = 10;
+const int updatePeriod = 5;
 unsigned long lastUpdate = 0;
 
 // Repeat time for Publish in seconds
@@ -40,10 +40,6 @@ time_t next_sync;
 
 time_t current_time;
 time_t next_pub;
-
-// table of Magnus coefficients for dew point calculation
-// [minTempC, maxTempC, C1(mbar), C2(-), C3(C)]
-float mgn[2][5] = {{-50.9,0,6.1078,17.84362,254.425},{0,100,6.1078,17.08085,234.175}};
 
 char buf[64];
 
@@ -77,25 +73,11 @@ void loop() {
     float pressure = (bme.readPressure() / 3386.39F); // inches-Hg
 
 		display.clearDisplay();
-		
+
+		// if we have good readings update screen & publish readings
 		if (!isnan(temp) && !isnan(humidity) && !isnan(pressure)) {
       // calculate dew point
-      int ms = -1;
-      float dewpoint = 999;
-      for (int i = 0; i < 2; i++) {
-        if (temp > mgn[i][0] && temp < mgn[i][1]) {
-          ms = i;
-        }
-      }
-      if (ms >= 0) {
-        // Magnus formula dewpoint calculation
-        float ps = mgn[ms][2] * exp((mgn[ms][3] * temp)/(mgn[ms][4] + temp));
-        float pd = ps * (humidity / 100);
-        dewpoint = ( (-log(pd / mgn[ms][2]) * mgn[ms][4]) / (log(pd / mgn[ms][2]) - mgn[ms][3]) );
-      }
-      else {
-        Particle.publish("error", "Temp out of range for dew point calculation.", PRIVATE);
-      }
+      float dewpoint = calcDewpoint(temp, humidity);
 
       // display results
 			display.setTextSize(2);
@@ -105,21 +87,21 @@ void loop() {
 			display.setCursor(0,0);
 			display.println(buf);
 
-			snprintf(buf, sizeof(buf), "%.1f %% RH", humidity);
+			snprintf(buf, sizeof(buf), "%.1f DwPtF", dewpoint * 9.0 / 5.0 + 32.0);
       display.setCursor(0,24);
 			display.println(buf);
 
-			// snprintf(buf, sizeof(buf), "%.1f InHg", pressure);
-      // display.setCursor(0,48);
+			// snprintf(buf, sizeof(buf), "%.1f %% RH", humidity);
+      // display.setCursor(0,24);
 			// display.println(buf);
 
-			snprintf(buf, sizeof(buf), "%.1f DwPtF", dewpoint * 9.0 / 5.0 + 32.0);
+			snprintf(buf, sizeof(buf), "%.1f InHg", pressure);
       display.setCursor(0,48);
 			display.println(buf);
 
       // Publish results
       if(current_time >= next_pub) {
-        String result = String::format("{\"Temp_C\": %4.2f, \"Press_InHg\": %4.2f, \"RelHum\": %4.2f}", temp, pressure, humidity);
+        String result = String::format("{\"Temp_C\": %4.2f, \"Dewpoint_C\": %4.2f, \"RelHum\": %4.2f, \"Press_InHg\": %4.2f}", temp, dewpoint, pressure, humidity);
         Particle.publish("readings", result, PRIVATE);
         next_pub = current_time - (current_time % period) + period;
       }
@@ -150,16 +132,46 @@ int current(String unit) {
       float temp = bme.readTemperature(); // degrees C
       float humidity = bme.readHumidity(); // % 
       float pressure = (bme.readPressure() / 3386.39F); // inches-Hg
-      String result = String::format("{\"Temp_C\": %4.2f, \"Press_InHg\": %4.2f, \"RelHum\": %4.2f}", temp, pressure, humidity);
+      float dewpoint = calcDewpoint(temp, humidity); // dew point C
+      result = String::format("{\"Temp_C\": %4.2f, \"Dewpoint_C\": %4.2f, \"RelHum\": %4.2f, \"Press_InHg\": %4.2f}", temp, dewpoint, pressure, humidity);
     }
     else if (unit == "f") {
-      float temp = (bme.readTemperature() * 9 / 5) + 32; // degrees F
+      float temp = bme.readTemperature();
+      float tempF = (temp * 9 / 5) + 32; // degrees F
       float humidity = bme.readHumidity(); // % 
       float pressure = (bme.readPressure() / 3386.39F); // inches-Hg
-      String result = String::format("{\"Temp_F\": %4.2f, \"Press_InHg\": %4.2f, \"RelHum\": %4.2f}", temp, pressure, humidity);
+      float dewpointF = (calcDewpoint(temp, humidity) * 9 / 5) + 32; // dew point F
+      result = String::format("{\"Temp_F\": %4.2f, \"Dewpoint_F\": %4.2f, \"RelHum\": %4.2f, \"Press_InHg\": %4.2f}", tempF, dewpointF, pressure, humidity);
     }
     Particle.publish("current_conditions", result, PRIVATE);
     digitalWrite(led1, LOW);
     return 1;
 }
-        
+
+float calcDewpoint(float temp, float humidity) {
+  // calculate dewpoint from temp & relative humidity
+  // temp in C, rel humidity as decimal, i.e. .715 for 71.5%
+  // using Magnus formula
+
+  // table of Magnus coefficients for dew point calculation
+  // [minTempC, maxTempC, C1(mbar), C2(-), C3(C)]
+  float mgn[2][5] = {{-50.9,0,6.1078,17.84362,254.425},{0,100,6.1078,17.08085,234.175}};
+
+  int ms = -1;
+  float dewpoint = 999;
+  for (int i = 0; i < 2; i++) {
+    if (temp > mgn[i][0] && temp < mgn[i][1]) {
+      ms = i;
+    }
+  }
+  if (ms >= 0) {
+    // Magnus formula dewpoint calculation
+    float ps = mgn[ms][2] * exp((mgn[ms][3] * temp)/(mgn[ms][4] + temp));
+    float pd = ps * (humidity / 100);
+    dewpoint = ( (-log(pd / mgn[ms][2]) * mgn[ms][4]) / (log(pd / mgn[ms][2]) - mgn[ms][3]) );
+  }
+  else {
+    Particle.publish("error", "Temp out of range for dew point calculation.", PRIVATE);
+  }
+  return dewpoint;
+}
